8e60447a682bc6de6d88aa7db8c00aab2f1682d0
mitani1993 <fytim55@gmail.com>
2021-11-07 12:55:50 +0900

    desplay subscribed service index

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("/hello", name="hello")
+     *
+     */
+    public function index()
+    {
+        $result = '<html><body>';
+        $result = '<h1>Subscribed Service</h1>';
+        $result .= '<ol>';
+        $arr = $this->getSubscribedServices();
+        foreach ($arr as $key => $value) {
+            $result .= '<li>' . $key . '</li>';
+        }
+        $result .= '</ol>';
+        $result .= '</body></html>';
+
+        return new Response($result);
+    }
+}
