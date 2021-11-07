34f1af76a3d896e398eb5758c12e31ffa8fa54ed
mitani1993 <fytim55@gmail.com>
2021-11-07 17:16:46 +0900

    create sample index page

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,11 +2,7 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -14,14 +10,10 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello", name="hello")
      */
-    public function index(Request $request, LoggerInterface $logger)
+    public function index()
     {
-        $data = array(
-            'name' => array('first' => 'Taro', 'second' => 'Yamada'),
-            'age' => 36, 'mail' => 'taro@yamada.kun'
-        );
-
-        $logger->info(serialize($data));
-        return new JsonResponse($data);
+        return $this->render('hello/index.html.twig', [
+            'controller_name' => 'Hello Controller',
+        ]);
     }
 }
