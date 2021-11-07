fe3fb2b16d26ad11aadb2b843eef79ff39aeedbd
mitani1993 <fytim55@gmail.com>
2021-11-07 19:29:52 +0900

    create person class

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,14 +18,24 @@ class HelloController extends AbstractController
      */
     public function index(Request $request)
     {
-        $form = $this->createFormBuilder()
-        ->add('input', TextType::class)
+        $person = new Person();
+        $person->setName('taro')
+            ->setAge(36)
+            ->setMail('taro@yamada.kun');
+
+        $form = $this->createFormBuilder($person)
+        ->add('name', TextType::class)
+        ->add('age', IntegerType::class)
+        ->add('mail', EmailType::class)
         ->add('save', SubmitType::class, ['label' => 'Click'])
         ->getForm();
 
         if ($request->getMethod() == 'POST') {
             $form->handleRequest($request);
-            $msg = 'こんにちは、' . $form->get('input')->getData() . 'さん';
+            $obj = $form->getData();
+            $msg = 'Name: ' . $obj->getName() . '<br>'
+                . 'Age: ' . $obj->getAge() . '<br>'
+                . 'Mail: ' . $obj->getMail();
         } else {
             $msg = 'お名前をどうぞ!';
         }
@@ -32,5 +45,45 @@ class HelloController extends AbstractController
             'message' => $msg,
             'form' => $form->createView(),
         ]);
+    }
+}
+
+class Person
+{
+    protected $name;
+    protected $age;
+    protected $mail;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+        return $this;
+    }
+
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+        return $this;
     }
 }
