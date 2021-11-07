493385253feefa049136fb940388b3df7a2bd19b
mitani1993 <fytim55@gmail.com>
2021-11-07 18:01:52 +0900

    add FormBuiler

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,16 +15,22 @@ class HelloController extends AbstractController
      */
     public function index(Request $request)
     {
+        $form = $this->createFormBuilder()
+        ->add('input', TextType::class)
+        ->add('save', SubmitType::class, ['label' => 'Click'])
+        ->getForm();
+
         if ($request->getMethod() == 'POST') {
-            $input = $request->request->get('input');
-            $msg = 'こんにちは、' . $input . 'さん';
+            $form->handleRequest($request);
+            $msg = 'こんにちは、' . $form->get('input')->getData() . 'さん';
         } else {
-            $msg = 'お名前は?';
+            $msg = 'お名前をどうぞ!';
         }
 
         return $this->render('hello/index.html.twig', [
             'title' => 'Hello',
             'message' => $msg,
+            'form' => $form->createView(),
         ]);
     }
 }
