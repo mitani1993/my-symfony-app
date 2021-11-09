3df960643b20d28bb04ba712f0a6e05016aeb9b8
mitani1993 <fytim55@gmail.com>
2021-11-09 21:54:13 +0900

    create FindForm class

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Person;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,5 +25,48 @@ class HelloController extends AbstractController
             'title' => 'Hello',
             'data' => $data,
         ]);
+    }
+
+    /**
+     * @Route("/find", name="find")
+     */
+    public function find(Request $request)
+    {
+        $formobj = new FindForm();
+        $form = $this->createFormBuilder($formobj)
+            ->add('find', TextType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $findstr = $form->getData()->getFind();
+            $repository = $this->getDoctrine()
+                ->getRepository(Person::class);
+            $result = $repository->find($findstr);
+        } else {
+            $result = null;
+        }
+
+        return $this->render('hello/find.html.twig',[
+            'title' => 'Hello',
+            'form' => $form->createView(),
+            'data' => $result,
+        ]);
+    }
+}
+
+class FindForm
+{
+    private $find;
+
+    public function getFind()
+    {
+        return $this->find;
+    }
+
+    public function setFind($find)
+    {
+        return $this->find = $find;
     }
 }
