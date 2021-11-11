ac439743dc2e14b1d612f58e8a227bcee825f2ac
mitani1993 <fytim55@gmail.com>
2021-11-11 21:40:18 +0900

    create update_action

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -67,6 +67,34 @@ class HelloController extends AbstractController
                 ]);
             }
     }
+
+    /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update(Request $request, Person $person)
+    {
+        $form = $this->createFormBuilder($person)
+            ->add('name', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('age', IntegerType::class)
+            ->add('save', SubmitType::class, array('label' => 'Click'))
+            ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $person = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($person);
+            $manager->flush();
+            return $this->redirect('/hello');
+        } else {
+            return $this->render('hello/create.html.twig',[
+                'title' => 'Hello',
+                'message' => 'Update Entity id=' . $person->getId(),
+                'form' => $form->createView(),
+            ]);
+        }
+    }
 }
 
 class FindForm
